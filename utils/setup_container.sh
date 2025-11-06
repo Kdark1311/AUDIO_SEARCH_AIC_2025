c7df1ff315d912a73b0b2064c4cb98578701dd7a
Kdark1311 <vkiet4999@gmail.com>
2025-11-06 20:51:53 +0700

    delete file

--- utils/setup_container.sh
+++ utils/setup_container.sh
@@ -28,7 +28,7 @@ docker run -d \
   -e "discovery.type=single-node" \
   -e "xpack.security.enabled=false" \
   -e "xpack.security.http.ssl.enabled=false" \
-  docker.elastic.co/elasticsearch/elasticsearch:8.7.0
+  docker.elastic.co/elasticsearch/elasticsearch:8.11.0
 
 # 4. Đợi Elasticsearch khởi động
 echo ""
